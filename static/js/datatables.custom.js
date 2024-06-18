b0939834f39dd538eb3345a72f3321655b15849d
Vladimir Hulagov <v.hulagov@yadro.com>
2024-06-18 12:19:44 +0300

    Updated Bootstrap version and switch to dark theme

--- static/js/datatables.custom.js
+++
@@ -1,8 +0,0 @@
-new DataTable('#tasks_table', {
-    columns: [
-        null,
-        { orderSequence: ['asc'] },
-        { orderSequence: ['desc', ''] }
-        //{ orderSequence: ['desc', 'asc', ''] }
-    ]
-});
